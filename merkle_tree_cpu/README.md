71d88c69fc4927d0d9440e3d0f8539c0bc866122
Pin-Tsung (Allen) Huang <allen.pt.huang@gmail.com>
2022-11-07 15:01:37 -0500

    Add ability to insert blocks. Add a README.

---
+++ merkle_tree_cpu/README.md
@@ -0,0 +1,5 @@
+# Merkle Tree - CPU Version
+A navie implementation of Merkle Tree.
+
+## Usage
+```./merkle_tree_cpu <filename>"```
--- merkle_tree_cpu/merkle_tree_cpu.cpp
+++ merkle_tree_cpu/merkle_tree_cpu.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <filesystem>
 #include <fstream>
 #include <queue>
@@ -26,6 +27,7 @@ class Blocks {
    vector<Block> const& blocks() {
     return _blocks;
    }
+   Blocks() {}
    Blocks(unsigned char* data, int data_len) {
     int num_of_blocks = data_len / BLOCK_SIZE;
     int offset = 0;
@@ -40,6 +42,10 @@ class Blocks {
       memcpy(&b.data, data + offset, data_len - offset);
       _blocks.push_back(b);
     }
+   }
+   void add_blocks(Blocks& new_blocks) {
+    _blocks.insert(_blocks.end(), new_blocks.blocks().begin(),
+                   new_blocks.blocks().end());
    }
 };
 
@@ -75,6 +81,7 @@ class MerkleNode {
 class MerkleTree {
  private:
   MerkleNode* root;
+  Blocks blocks;
 
  public:
   void print() {
@@ -126,7 +133,29 @@ class MerkleTree {
     return cur_layer_nodes[0];
   }
 
-  MerkleTree(Blocks& blocks) {
+  MerkleTree(Blocks& blocks_) {
+    blocks = blocks_;
+    root = make_tree_from_blocks(blocks);
+  }
+
+  void delete_tree_walker(MerkleNode* cur_node) {
+    if (cur_node == nullptr) {
+      return;
+    }
+    delete_tree_walker(cur_node->left);
+    delete_tree_walker(cur_node->right);
+    delete(cur_node);
+  }
+
+  void delete_tree() {
+    delete_tree_walker(root);
+    root = nullptr;
+  }
+
+  // TODO(allenpthuang): Naive way to insert blocks! Should be more efficient.
+  void insert(Blocks& new_blocks) {
+    blocks.add_blocks(new_blocks);
+    delete_tree();
     root = make_tree_from_blocks(blocks);
   }
 };
@@ -135,30 +164,53 @@ int main(int argc, char *argv[]) {
   unsigned char* data;
   int data_len = 0;
   if (argc == 1) {
+    // no input file; use dummy data for demo.
     cerr << "Usage: ./merkle_tree_cpu <filename>" << endl;
-    cerr << "For demo, creating data filed with '9527' with 4096 bytes." << endl;
-    data = (unsigned char*) malloc(BLOCK_SIZE * 4 * sizeof(unsigned char));
+    cerr << "For demo, create data filed with '9527' with 4096 bytes." << endl;
+    data = (unsigned char *)malloc(BLOCK_SIZE * 4 * sizeof(unsigned char));
     data_len = BLOCK_SIZE * 4;
     memset(data + BLOCK_SIZE * 0, 9, BLOCK_SIZE);
     memset(data + BLOCK_SIZE * 1, 5, BLOCK_SIZE);
     memset(data + BLOCK_SIZE * 2, 2, BLOCK_SIZE);
     memset(data + BLOCK_SIZE * 3, 7, BLOCK_SIZE);
   } else {
+    // input filepath provided
     fs::path p{argv[1]};
     if (! fs::exists(p)) {
       cerr << "File not found at: " << fs::absolute(p) << endl;
       exit(2);
     }
+    // show file info and read into a buffer
     cout << "path = " << fs::absolute(p) << endl;
     cout << "filesize = " << fs::file_size(p) << endl;
     data_len = fs::file_size(p);
-    data = (unsigned char*) malloc(data_len * sizeof(unsigned char));
+    data = (unsigned char *)malloc(data_len * sizeof(unsigned char));
     ifstream is;
     is.open(p, ios::binary);
     is.read((char*)data, data_len);
   }
+
+  // make blocks and make a merkle tree from them
   Blocks blocks(data, data_len);
   MerkleTree merkle_tree(blocks);
+  cout << "===== Read all at once. =====" << endl;
   merkle_tree.print();
+
+  // split input data into two halves; the second half is inserted later.
+  int num_of_blocks = ceil((double)data_len / BLOCK_SIZE);
+  if (num_of_blocks > 1) {
+    int first_size = BLOCK_SIZE * (num_of_blocks / 2);
+    Blocks old_blocks(data, first_size);
+    MerkleTree merkle_tree_to_insert(old_blocks);
+    cout << "===== Read half first, and append the other half. =====" << endl;
+    cout << "=== Merkle Tree of the first half ===" << endl;
+    merkle_tree_to_insert.print();
+
+    Blocks new_blocks(data + first_size, data_len - first_size);
+    merkle_tree_to_insert.insert(new_blocks);
+    cout << "=== Merkle Tree of the first half + the second half ===" << endl;
+    merkle_tree_to_insert.print();
+  }
+
   return 0;
 }
