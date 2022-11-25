350b2675339b79817103a01fe84957b192025380
Pin-Tsung (Allen) Huang <allen.pt.huang@gmail.com>
2022-11-24 23:43:24 -0500

    Add a test script.

---
+++ run_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ ! -x ./bin/benchmark_cpu ]] || [[ ! -x ./bin/benchmark_gpu ]]; then
+    echo "Building..."
+    make
+    if [[ $? != 0 ]]; then
+        exit $?
+    fi
+fi
+
+echo "Running tests..."
+data_lens=(100 1000 10000 100000 1000000 10000000 100000000)
+block_sizes=(10 100 1000 10000)
+for data_len in ${data_lens[@]}; do
+    for block_size in ${block_sizes[@]}; do
+        echo -n "CPU: "
+        ./bin/benchmark_cpu ${data_len} ${block_size}
+        echo -n "GPU: "
+        ./bin/benchmark_cpu ${data_len} ${block_size}
+    done
+done
